88a36c06875fdea80b989f3d7c7a0758d48be7f5
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-01-17 23:40:38 -0700

    favorite reads model schema added

---
+++ models/favoriteReads.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const favesSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: { type: String, required: true },
+    img: { type: String, required: true },
+    description: { type: String, required: true },
+    year: { type: Number, required: false }
+})
+
+const FaveReads = mongoose.model("faves", favesSchema)
+
+module.exports = FaveReads
